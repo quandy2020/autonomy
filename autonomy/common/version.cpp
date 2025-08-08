0ed4d8343d6246a5cb028cfa94c75ef53756c61f
quandy <quandy@quandydeMacBook-Pro.local>
2025-08-08 11:45:41 +0800

    [Add] Add controller
    	1 add controllers
    
    Signed-off-by: quandy <quandy@quandydeMacBook-Pro.local>

--- autonomy/common/version.cpp
+++ autonomy/common/version.cpp
@@ -25,7 +25,7 @@ namespace common {
 namespace {
 
 const char* AUTONOMY_VERSION = "0.0.1";
-const char* AUTONOMY_COMMIT_ID = "884e9de";
+const char* AUTONOMY_COMMIT_ID = "2fc187d";
 const char* AUTONOMY_COMMIT_DATE = "2025-08-07";
 
 }  // namespace
