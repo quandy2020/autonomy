0fe70c5d193c811527454d8393c4b7ad09dde9be
duyongquan <quandy2020@126.com>
2025-08-12 11:44:40 +0800

    [Fix] Add grid_map
    	1 add grid_map
    
    Signed-off-by: duyongquan <quandy2020@126.com>

---
+++ autonomy/map/grid_map/grid_map_core/grid_map_core.hpp
@@ -0,0 +1,18 @@
+/*
+ * grid_map_core.hpp
+ *
+ *  Created on: Jul 14, 2014
+ *      Author: Péter Fankhauser
+ *	 Institute: ETH Zurich, ANYbotics
+ */
+
+#pragma once
+
+#include "autonomy/map/grid_map/grid_map_core/type_defs.hpp"
+#include "autonomy/map/grid_map/grid_map_core/grid_map.hpp"
+#include "autonomy/map/grid_map/grid_map_core/submap_geometry.hpp"
+#include "autonomy/map/grid_map/grid_map_core/grid_map_math.hpp"
+#include "autonomy/map/grid_map/grid_map_core/buffer_region.hpp"
+#include "autonomy/map/grid_map/grid_map_core/polygon.hpp"
+#include "autonomy/map/grid_map/grid_map_core/iterators/iterators.hpp"
+#include "autonomy/map/grid_map/grid_map_core/eigen_plugins/functors.hpp"
