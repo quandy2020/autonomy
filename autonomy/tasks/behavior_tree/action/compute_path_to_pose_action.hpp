2e7ce7b930786bb9db801768000804495102c94c
duyongquan <quandy2020@126.com>
2025-08-22 13:52:42 +0800

    fix
    Signed-off-by: duyongquan <quandy2020@126.com>

--- autonomy/tasks/behavior_tree/action/compute_path_to_pose_action.hpp
+++ autonomy/tasks/behavior_tree/action/compute_path_to_pose_action.hpp
@@ -18,74 +18,74 @@
 
 #include "autonomy/tasks/behavior_tree/action/bt_action.hpp"
 
-#include "autonomy/tasks/navigation/proto/compute_path_to_pose.pb.h"
+// #include "autonomy/tasks/navigation/proto/compute_path_to_pose.pb.h"
 
 namespace autonomy {
 namespace tasks {
 namespace behavior_tree {
 namespace action {
 
-/**
- * @brief A nav2_behavior_tree::BtActionNode class that wraps nav2_msgs::action::ComputePathToPose
- */
-class ComputePathToPoseAction : public BtActionNode<navigation::proto::ComputePathToPose>
-{
-    // using Action = nav2_msgs::action::ComputePathToPose;
-    // using ActionResult = Action::Result;
+// /**
+//  * @brief A nav2_behavior_tree::BtActionNode class that wraps nav2_msgs::action::ComputePathToPose
+//  */
+// class ComputePathToPoseAction : public BtActionNode<navigation::proto::ComputePathToPose>
+// {
+//     // using Action = nav2_msgs::action::ComputePathToPose;
+//     // using ActionResult = Action::Result;
 
-public:
-    /**
-     * @brief A constructor for nav2_behavior_tree::ComputePathToPoseAction
-     * @param xml_tag_name Name for the XML tag for this node
-     * @param action_name Action name this node creates a client for
-     * @param conf BT node configuration
-     */
-    ComputePathToPoseAction(
-        const std::string& xml_tag_name, 
-        const std::string& action_name, 
-        const BT::NodeConfiguration& conf);
+// public:
+//     /**
+//      * @brief A constructor for nav2_behavior_tree::ComputePathToPoseAction
+//      * @param xml_tag_name Name for the XML tag for this node
+//      * @param action_name Action name this node creates a client for
+//      * @param conf BT node configuration
+//      */
+//     ComputePathToPoseAction(
+//         const std::string& xml_tag_name, 
+//         const std::string& action_name, 
+//         const BT::NodeConfiguration& conf);
 
-    /**
-     * @brief Function to perform some user-defined operation on tick
-     */
-    void on_tick() override;
+//     /**
+//      * @brief Function to perform some user-defined operation on tick
+//      */
+//     void on_tick() override;
 
-    /**
-     * @brief Function to perform some user-defined operation upon successful completion of the action
-     */
-    BT::NodeStatus on_success() override;
+//     /**
+//      * @brief Function to perform some user-defined operation upon successful completion of the action
+//      */
+//     BT::NodeStatus on_success() override;
 
-    /**
-     * @brief Function to perform some user-defined operation upon abortion of the action
-     */
-    BT::NodeStatus on_aborted() override;
+//     /**
+//      * @brief Function to perform some user-defined operation upon abortion of the action
+//      */
+//     BT::NodeStatus on_aborted() override;
 
-    /**
-     * @brief Function to perform some user-defined operation upon cancellation of the action
-     */
-    BT::NodeStatus on_cancelled() override;
+//     /**
+//      * @brief Function to perform some user-defined operation upon cancellation of the action
+//      */
+//     BT::NodeStatus on_cancelled() override;
 
-    /**
-     * \brief Override required by the a BT action. Cancel the action and set the path output
-     */
-    void halt() override;
+//     /**
+//      * \brief Override required by the a BT action. Cancel the action and set the path output
+//      */
+//     void halt() override;
 
-    /**
-     * @brief Creates list of BT ports
-     * @return BT::PortsList Containing basic ports along with node-specific ports
-     */
-    static BT::PortsList providedPorts()
-    {
-        return providedBasicPorts(
-        {
-            BT::InputPort<commsgs::geometry_msgs::PoseStamped>("goal", "Destination to plan to"),
-            BT::InputPort<commsgs::geometry_msgs::PoseStamped>("start", "Start pose of the path if overriding current robot pose"),
-            BT::InputPort<std::string>("planner_id", "", "Mapped name to the planner plugin type to use"),
-            BT::OutputPort<commsgs::planning_msgs::Path>("path", "Path created by ComputePathToPose node"),
-            // BT::OutputPort<ActionResult::_error_code_type>("error_code_id", "The compute path to pose error code"),
-        });
-    }
-};
+//     /**
+//      * @brief Creates list of BT ports
+//      * @return BT::PortsList Containing basic ports along with node-specific ports
+//      */
+//     static BT::PortsList providedPorts()
+//     {
+//         return providedBasicPorts(
+//         {
+//             BT::InputPort<commsgs::geometry_msgs::PoseStamped>("goal", "Destination to plan to"),
+//             BT::InputPort<commsgs::geometry_msgs::PoseStamped>("start", "Start pose of the path if overriding current robot pose"),
+//             BT::InputPort<std::string>("planner_id", "", "Mapped name to the planner plugin type to use"),
+//             BT::OutputPort<commsgs::planning_msgs::Path>("path", "Path created by ComputePathToPose node"),
+//             // BT::OutputPort<ActionResult::_error_code_type>("error_code_id", "The compute path to pose error code"),
+//         });
+//     }
+// };
 
 }  // namespace action
 }  // namespace behavior_tree
