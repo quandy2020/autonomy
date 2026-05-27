a17d7e08d40ebf2b9a0cd579c654a1d8a28da0fd
duyongquan <quandy2020@126.com>
2026-05-28 02:19:16 +0800

    [fix] fix version.cpp
        - fix version.cpp

--- autonomy/common/version.cpp
+++ autonomy/common/version.cpp
@@ -93,12 +93,12 @@ const std::string PROJECT_VERSION = "";
 // Git提交信息
 const std::string GIT_COMMIT_AUTHOR = "duyongquan";
 const std::string GIT_COMMIT_EMAIL = "quandy2020@126.com";
-const std::string GIT_COMMIT_DATE = """2026-05-28 00:42:03""";
-const std::string GIT_VERSION = "8a2115cf-dirty";
+const std::string GIT_COMMIT_DATE = """2026-05-28 01:54:31""";
+const std::string GIT_VERSION = "6502cc4f-dirty";
 const std::string GIT_BRANCH = "feature/library";
 
 // 构建信息
-const std::string BUILD_TIMESTAMP = "2026-05-27 17:54:41";
+const std::string BUILD_TIMESTAMP = "2026-05-27 17:55:14";
 const std::string BUILD_HOST = "9b2023bccf6b";
 const std::string BUILD_USER = "root";
 
