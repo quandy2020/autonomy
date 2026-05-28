e08ac81ec2d23ab31ecd3e6d36f5c822fe4a0c8c
duyongquan <quandy2020@126.com>
2026-05-28 11:28:53 +0800

    [feat] update README.md
        - update README.md

--- autonomy/common/version.cpp
+++ autonomy/common/version.cpp
@@ -93,12 +93,12 @@ const std::string PROJECT_VERSION = "";
 // Git提交信息
 const std::string GIT_COMMIT_AUTHOR = "duyongquan";
 const std::string GIT_COMMIT_EMAIL = "quandy2020@126.com";
-const std::string GIT_COMMIT_DATE = """2026-05-28 08:39:41""";
-const std::string GIT_VERSION = "49a20e74";
+const std::string GIT_COMMIT_DATE = """2026-05-28 11:13:24""";
+const std::string GIT_VERSION = "eff435ef";
 const std::string GIT_BRANCH = "feature/library";
 
 // 构建信息
-const std::string BUILD_TIMESTAMP = "2026-05-28 01:48:00";
+const std::string BUILD_TIMESTAMP = "2026-05-28 03:27:09";
 const std::string BUILD_HOST = "9b2023bccf6b";
 const std::string BUILD_USER = "root";
 
