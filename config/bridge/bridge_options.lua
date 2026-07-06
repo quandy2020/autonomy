de8b4147aed510f08ec1b100857d91941a6b2159
duyongquan <quandy2020@126.com>
2026-07-06 10:14:58 +0800

    [feat] update

---
+++ config/bridge/bridge_options.lua
@@ -0,0 +1,8 @@
+-- Copyright 2025 The Openbot Authors(duyongquan)
+--
+-- Entry Lua for autonomy_bridge (see autonomy/bridge/launch/bridge.launch).
+
+include "common.lua"
+include "bridge.lua"
+
+return AUTONOMY_BRIDGE
