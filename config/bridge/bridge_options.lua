032fcf1a721bb0d03f02694ee85aa63e22043fc0
duyongquan <quandy2020@126.com>
2026-09-14 17:21:35 +0800

    [feat] update

--- config/bridge/bridge_options.lua
+++
@@ -1,8 +0,0 @@
--- Copyright 2025 The Openbot Authors(duyongquan)
---
--- Entry Lua for autonomy.bridge (see autonomy/bridge/launch/bridge.launch).
-
-include "common.lua"
-include "bridge/bridge.lua"
-
-return AUTONOMY_BRIDGE
--- config/task_options.lua
+++
@@ -1,7 +0,0 @@
--- Copyright 2025 The Openbot Authors(duyongquan)
---
--- Task server options. For navigator-only legacy setups see navigator/navigator.lua.
-
-include "task/task_options.lua"
-
-return task
